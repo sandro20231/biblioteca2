0638bc4c2d5138b897bcbaaafc82da5f22d5931d
Sandro <sandrocoelho2023a@gmail.com>
2025-05-27 11:14:15 -0300

    criando tela inicial, telas de cadastro e listagem

--- biblioteca2/biblioteca/urls.py
+++ biblioteca2/biblioteca/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('', views.index, name="index"),
+    path('inserirautor/', views.inserirautor, name="inserirautor"),
+    path('inserirlivro/', views.inserirlivro, name="inserirlivro"),
+    path('login/', views.login, name="login"),
+    path('listarautores/', views.listarautores, name="listarautores"),
+    path('listarlivros', views.listarlivros, name="listarlivros")
 ]
--- biblioteca2/biblioteca/views.py
+++ biblioteca2/biblioteca/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'biblioteca/index.html')
+
+
+def inserirautor(request):
+    return render(request, 'biblioteca/inserirautor.html')
+
+
+def inserirlivro(request):
+    return render(request, 'biblioteca/inserirlivro.html')
+
+
+def login(request):
+    return render(request, 'biblioteca/login.html')
+
+
+def listarautores(request):
+    return render(request, 'biblioteca/listaauroes.html')
+
+
+def listarlivros(request):
+    return render(request, 'biblioteca/listarlivros.html')
