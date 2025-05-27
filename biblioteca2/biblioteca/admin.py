adb0126914d67d34b0dc5c160496df8863158d8a
Sandro <sandrocoelho2023a@gmail.com>
2025-05-27 00:02:15 -0300

    criando e configurando modelos

--- biblioteca2/biblioteca/admin.py
+++ biblioteca2/biblioteca/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Autor, Livro
 # Register your models here.
+admin.site.register(Autor)
+admin.site.register(Livro)
--- biblioteca2/biblioteca/models.py
+++ biblioteca2/biblioteca/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=64)
+    idade = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.nome} - {self.idade}"
+
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=64)
+    autor = models.ForeignKey(
+        Autor, on_delete=models.CASCADE, related_name="objetoautor")
+    resumo = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return f"{self.titulo} - {self.autor} - {self.resumo}"
